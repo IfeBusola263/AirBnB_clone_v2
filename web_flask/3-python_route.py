99c418e2cfacc7078e34402372392a115f0bbe09
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-22 18:47:09 +0000

    Included the route for python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+'''
+This is module starts a flask web application
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''
+    The serves the content in this route
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def home():
+    '''
+    The serves the content in /hbnb folder
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    '''
+    The serves the content with a variable
+    '''
+    if '_' in text:
+        new_text = text.replace('_', ' ')
+        return f'C {new_text}'
+    else:
+        return f'C {text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_c(text):
+    '''
+    The serves the content with a variable
+    '''
+    if text and '_' in text:
+        new_text = text.replace('_', ' ')
+        return f'Python {new_text}'
+    else:
+        return 'Python is cool'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
