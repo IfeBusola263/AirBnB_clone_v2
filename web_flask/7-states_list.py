eeef82a936ea69d05642e6c7ed3c7d047ab0f852
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-25 11:02:29 +0000

    State list from data base using jinja

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module starts a flask web -application
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """This function returns the list of states in the database"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_session():
+    """This function closes the current session after the request is completed
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
